5aedc2b11d437069f3e87193496e2814434eee56
Chloe (Sijia) Hu <150668599+ChloeHu97@users.noreply.github.com>
2024-05-05 00:34:12 +1200

    Update Linkedin link

--- assets/js/app.js
+++ assets/js/app.js
@@ -157,8 +157,8 @@ let footer = $(`
     
 
     <div class="rounded-social-buttons tag" align="center">
-
-    <a class="social-button linkedin" href="www.linkedin.com/in/sijia-chloe-hu" target="_blank" >
+   
+    <a class="social-button linkedin" href="https://www.linkedin.com/in/sijia-chloe-hu" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
